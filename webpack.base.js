bccb6ce4e0ecaf6dce380b242cbf0f9f5d3c38cc
jimi-wzq <wangzhengquan85@gmail.com>
2019-12-25 20:39:34 +0800

    update

--- webpack.base.js
+++ webpack.base.js
@@ -19,6 +19,8 @@ module.exports = {
         filename: '[name].[hash].js',
         path: defaultConfig.distDir,
         publicPath: '/'
+      //静态文件展现时用下面的路径
+        //publicPath: './'
     },
 
     
